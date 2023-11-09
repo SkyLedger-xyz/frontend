38a68bf652be08480793043e74c01f2ce689b3c3
Gabriel Olaniyi Aborisade <42632188+Nejo12@users.noreply.github.com>
2023-11-09 13:51:36 +0100

    feat: custom inputs and icons
    - icons are placed in assets folder.
    - inputs require sizes (s, m and l), boolean icon position (icon_to_right) and wip on responsiveness

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,25 @@
+import Input from "./Input"
 import Content from "./Layouts/Content"
-import ShinnyButton from './ShinnyButton'
+
+import { Icon } from "./utils/IconsUtils"
 
 const Main = () =>
   <div className="main">
     <Content>
-      <ShinnyButton text='The&nbsp;Journey&nbsp;Begins' />
+      <div className="flex-row">
+        <Input placeholder='02.11.23' icon={ Icon('50d.png') } input_size='s' />
+        <Input icon={ Icon('unknown.png') } input_size='m' icon_to_right />
+        <Input icon={ Icon('01d.png') } input_size='m' />
+        <Input icon={ Icon('03d.png') } input_size='m' icon_to_right />
+        <Input icon={ Icon('04d.png') } input_size='s' />
+        <Input icon={ Icon('04n.png') } input_size='m' icon_to_right />
+        <Input icon={ Icon('11d.png') } input_size='m' />
+        <Input icon={ Icon('13d.png') } input_size='m' icon_to_right />
+        <Input icon={ Icon('13n.png') } input_size='l' />
+        <Input icon={ Icon('10d.png') } input_size='l' icon_to_right />
+        <Input icon={ Icon('10n.png') } input_size='l' />
+        <Input icon={ Icon('10d.png') } input_size='l' icon_to_right />
+      </div>
     </Content>
   </div>
 
